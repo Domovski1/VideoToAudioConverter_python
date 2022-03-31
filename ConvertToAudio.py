924c3ca1b66930ab3308ca58a3487cbf984c422e
Domovski1 <624273242@mail.ru>
2022-03-31 06:47:55 +0300

    Добавил функцию, превращающая время в секунды

--- ConvertToAudio.py
+++ ConvertToAudio.py
@@ -13,7 +13,10 @@ def ConvertToMp3(mp4file, mp3file):
     video.close()
     print('[INFO] Видео было конвертировано')
 
-def converttomp3Double(mp4file, mp3file, starttime, endtime):
+
+
+
+def CutAndConvert(mp4file, mp3file, starttime, endtime):
     """Метод, позволяющий преобразовывать видео в аудио"""
     video=VideoFileClip(mp4file).subclip(starttime, endtime)
     # получаем аудиодорожку
@@ -27,6 +30,8 @@ def converttomp3Double(mp4file, mp3file, starttime, endtime):
     print('[INFO] Видео было конвертировано')
 
 
+
+
 def MoveFileToSource(path: str):
     """Перемешает полученный результат туда, где находится оригинал видео"""
     path = path.replace("MP4", "mp3")
@@ -35,6 +40,36 @@ def MoveFileToSource(path: str):
     print(move_to)
     # shutil.move(path, move_to)
     print("[INFO] File has been moved")
+
+
+
+def ConvertTimeToSeconds(insert_time):
+    """Получает время на вход и возвращает результат в секундах"""
+    all_time = insert_time.split(":")
+
+    if len(all_time) > 2:
+        if int(all_time[1]) > 60 or int(all_time[2] > 60):
+            print("Входные данные имели неверные формат")
+            exit()
+        hour = int(all_time[0]) * 3600
+        minutes = int(all_time[1]) * 60
+        seconds = int(all_time[2])
+        return hour + minutes + seconds
+    elif len(all_time) > 1:
+        if int(all_time[0]) > 60 or int(all_time[1]) > 60:
+            print("Входные данные имели неверные формат")
+            exit()
+        minutes = int(all_time[0]) * 60
+        seconds = int(all_time[1])
+        result_time = minutes + seconds
+        return result_time
+    else:
+        seconds = all_time[0]
+        if seconds > 60:
+            print("неверно указано время")
+            exit()
+        result_time = seconds
+        return result_time
 
 
 print('Вставьте путь к файлу:')
@@ -46,16 +81,26 @@ path_to_file = "/mnt/" + path_to_file
 file_name = path_to_file.split("/")
 final_name = file_name[-1].replace("mp4", "mp3").replace("MP4", "mp3")
 
+print('1 - Чтобы вырезать время и конвертировать его')
+print('2 - Чтобы конвертировать целиком')
 result = input()
+
+
 if (result == "1"):
     # Обработка видео в аудио и перемешение его туда, где находится основной файл
-    print("Start time in seconds: ")
+    print("Введите время, с которой нужно начать обрезать видео: ")
     starttime = input()
+    starttime = ConvertTimeToSeconds(starttime)
+    
+
     print("End time in seconds: ")
     endtime = input()
-    converttomp3Double(path_to_file, final_name, starttime, endtime)
+    endtime = ConvertTimeToSeconds(endtime)
+    CutAndConvert(path_to_file, final_name, starttime, endtime)
     move_to = path_to_file.replace(file_name[-1], "")
     shutil.move(final_name, move_to)
+
+
 else:
     # Обработка видео в аудио и перемешение его туда, где находится основной файл
     ConvertToMp3(path_to_file, final_name)
